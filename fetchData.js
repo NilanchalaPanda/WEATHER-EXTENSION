4234f04ef7317e7dbbe5923484e84d7399db95df
Nilanchal <nilanchalpanda2003@gmail.com>
2023-12-01 08:28:58 +0530

    Image fetched and displayed & Location accessed

--- fetchData.js
+++ fetchData.js
@@ -1,17 +1,23 @@
 const API_KEY = "65756b6c72f33dcdf36e274a5f5010d8";
 
-const fetchData = async () => {
+// GEOLOCATION API USED : 
+function success(position){
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  fetchData(lat, lon);
+}
+
+// OPEN WETHER API USED : 
+const fetchData = async (lat, lon) => {
   try {
+
     const weatherData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
     );
 
-    if (!weatherData.ok) throw new Error("Data was not fetch properly");
+    if (!weatherData.ok) throw new Error("Data was not fetched properly");
 
     const result = await weatherData.json();
-
-    // const weatherIcon = await fetch(`https://api.openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`)
-    // const finalImg = weatherIcon.blob();
 
     document.getElementById('text_location').innerHTML = result.name
     document.getElementById('text_location_country').innerHTML = result.sys.country
@@ -20,12 +26,12 @@ const fetchData = async () => {
     document.querySelector('.text_tempFeelsLike').innerHTML = Math.round(result.main.feels_like)
     document.getElementById('text_desc').innerHTML = result.weather[0].description
 
-    // const imageObjectURL = URL.createObjectURL(finalImg);
-    // document.querySelector('.icon').src = imageObjectURL;
+    const img = `https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`
+    document.querySelector('.icon').src = img;
     
   } catch (err) {
     console.log("Error : ", err);
   }
 };
 
-fetchData();
+navigator.geolocation.getCurrentPosition(success);
